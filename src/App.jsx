088b7d8d394cbd1f186a0e78e204988766a494df
kienntheprfs <kien.trantrung@hcmut.edu.vn>
2024-08-31 23:13:29 +0700

    add local storage to store to-do list's data atfer reloading the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
 
@@ -14,23 +14,50 @@ export default function App() {
   //React store variables in a parent-child relationship, so if the state is stored in the parent component, 
   // it can be passed down and access from the child component
   const [todoValue, setTodoValue] = useState('')
+  
+  
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({todos: newList}))
+  }
+  
   //Any elements in a React component must be immutable, only the state is mutable
   const handleAddTodos = (todo) => { 
-    setTodos([...todos, todo])
+    let newTodoList = [...todos, todo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
     setTodoValue('')
+
   }
   
   const handleDeleteTodos = (index) => {
-    const newTodos = todos.filter((todo, i) => {
+    const newTodoList = todos.filter((todo, i) => {
       return i !== index
     })
-    setTodos(newTodos)
+    persistData(newTodoList)
+    setTodos(newTodoList)
   }
   const handleEditTodos = (index) => {
     const todoTobeEdited = todos[index]
     setTodoValue(todoTobeEdited)
     handleDeleteTodos(index)
   }
+
+// useEffect listen for events such as page loading, states changed,... 
+// and perform actions based on when those events happen
+// In this case, we use useEffect to to get the todos from the local storage (if has any)
+// whenever the page is loaded
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+
+  }, [])
 
   return (
     <> 
